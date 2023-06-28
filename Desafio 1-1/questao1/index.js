c2ce1a5a9dd4856b9f6799b2af51b86511651c4d
Darlysson <don@ic.ufal.br>
2023-06-28 08:47:13 -0300

    Uploaded questao1/index.js (1.1 KB) to main. ```

--- Desafio 1-1/questao1/index.js
+++ Desafio 1-1/questao1/index.js
@@ -31,21 +31,9 @@ vertice1 = new Vertice(vertice1[0], vertice1[1]);
 vertice2 = new Vertice(vertice2[0], vertice2[1]);
 vertice3 = new Vertice(vertice3[0], vertice3[1]);
 
-switch (prompt('Digite 1 para calcular a distância entre os vértices 1 e 2, 2 para calcular a distância entre os vértices 2 e 3 e 3 para calcular a distância entre os vértices 1 e 3: ')) {
-    case '1':
-        console.log(`A distância entre o vértices 1 e 2 é ${vertice1.distancia(vertice2)}`);
-        break;
-    case '2':
-        console.log(`A distância entre o vértices 2 e 3 é ${vertice2.distancia(vertice3)}`);
-        break;
-    case '3':
-        console.log(`A distância entre o vértices 1 e 3 é ${vertice1.distancia(vertice3)}`);
-        break;
-    default:
-        console.log('Opção inválida');
-        break;
-}
-
+console.log(`A distância entre o vértices 1 e 2 é ${vertice1.distancia(vertice2)}`);
+console.log(`A distância entre o vértices 2 e 3 é ${vertice2.distancia(vertice3)}`);
+console.log(`A distância entre o vértices 1 e 3 é ${vertice1.distancia(vertice3)}`);
 
 
 switch (prompt('Digite 1 para mover o vértice 1, 2 para mover o vértice 2 e 3 para mover o vértice 3: ')) {
@@ -63,17 +51,8 @@ switch (prompt('Digite 1 para mover o vértice 1, 2 para mover o vértice 2 e 3 
         break;
 }
 
-switch (prompt('Digite 1 para verificar se o vértice 1 é igual ao vértice 2, 2 para verificar se o vértice 2 é igual ao vértice 3 e 3 para verificar se o vértice 1 é igual ao vértice 3: ')) {
-    case '1':
-        console.log(`O vértice 1 é igual ao vértice 2? ${vertice1.equals(vertice2)}`);
-        break;
-    case '2':
-        console.log(`O vértice 2 é igual ao vértice 3? ${vertice2.equals(vertice3)}`);
-        break;
-    case '3':
-        console.log(`O vértice 1 é igual ao vértice 3? ${vertice1.equals(vertice3)}`);
-        break;
-    default:
-        console.log('Opção inválida');
-        break;
-}
+console.log(`O vértice 1 é igual ao vértice 2? ${vertice1.equals(vertice2)}`);
+console.log(`O vértice 2 é igual ao vértice 3? ${vertice2.equals(vertice3)}`);
+console.log(`O vértice 1 é igual ao vértice 3? ${vertice1.equals(vertice3)}`);
+
+
