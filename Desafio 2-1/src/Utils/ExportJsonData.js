605f8dffd3073b236a65d22889662518a82787dc
Darlysson <don@ic.ufal.br>
2023-07-25 15:46:52 -0300

    Documentation

--- Desafio 2-1/src/Utils/ExportJsonData.js
+++ Desafio 2-1/src/Utils/ExportJsonData.js
@@ -2,14 +2,14 @@ import { DateTime } from 'luxon';
 import fs from 'fs';
 import GetInvalidData from './GetInvalidData.js';
 import path from 'path';
-
+// Responsável por exportar os dados inválidos para um arquivo JSON no formato especificado
 function ExportJsonData(data){
     
     const pasta = path.resolve('Json');
     const arquivoSaida = path.join(pasta,'erros-' + DateTime.now().toFormat('ddMMyyyy-HHmmss') + '.json'); // formatando nome do arquivo de saída com o timestamp atual
-
+    // Pega os dados dos clientes com erros
     let clientesInvalidos = GetInvalidData(data);
-
+    //Escreve os erros, se existir
     if (clientesInvalidos.length > 0) {
         try {
             fs.writeFileSync(arquivoSaida, JSON.stringify(clientesInvalidos));
@@ -19,6 +19,7 @@ function ExportJsonData(data){
             process.exit(1);
         }
     }
+    // Se não existir erros, cria um arquivo vazio
     else {
         try {
             fs.writeFileSync(arquivoSaida, JSON.stringify([]));
